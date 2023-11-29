e5d8840bec6d1879287d8b5c7dc873aaf671611b
Bhargav-sdet1 <bhargav.sdet1@gmail.com>
2023-11-29 23:26:26 +0530

    29/11

---
+++ JavaPrograms/src/javaPractise/EvenOdd.java
@@ -0,0 +1,17 @@
+package javaPractise;
+
+public class EvenOdd {
+
+	public static void main(String[] args) {
+
+		int num=5;
+		
+		if(num%2==0) {
+			System.out.println(num+" is Even Number");
+		}
+		else
+			System.out.println(num+" is Odd Number");
+
+	}
+
+}
