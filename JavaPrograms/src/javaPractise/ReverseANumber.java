161f5b7a80e0d247503d08b190c5b082c9951cc3
Bhargav-sdet1 <bhargav.sdet1@gmail.com>
2023-11-30 23:50:53 +0530

    30/11

---
+++ JavaPrograms/src/javaPractise/ReverseANumber.java
@@ -0,0 +1,19 @@
+package javaPractise;
+
+public class ReverseANumber {
+
+	public static void main(String[] args) {
+
+		int num=12231;
+		
+		int rev=0,rem;
+		
+		while(num!=0) {
+			rem=num%10;
+			rev=rev*10+rem;
+			num=num/10;
+		}
+		System.out.println(rev);
+	}
+
+}
