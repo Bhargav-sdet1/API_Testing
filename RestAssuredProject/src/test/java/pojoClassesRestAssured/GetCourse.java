2a20474c6d9ac087b724b768b186dfe64eaacaa1
Bhargav-sdet1 <bhargav.sdet1@gmail.com>
2023-12-05 22:31:25 +0530

    Deserialization

---
+++ RestAssuredProject/src/test/java/pojoClassesRestAssured/GetCourse.java
@@ -0,0 +1,52 @@
+package pojoClassesRestAssured;
+
+public class GetCourse {
+
+	private String url;
+	private String services;
+	private String expertise;
+	private String instructor;
+	private String linkedIn;
+	private Courses courses;
+	
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public String getServices() {
+		return services;
+	}
+	public void setServices(String services) {
+		this.services = services;
+	}
+	public String getExpertise() {
+		return expertise;
+	}
+	public void setExpertise(String expertise) {
+		this.expertise = expertise;
+	}
+	public String getInstructor() {
+		return instructor;
+	}
+	public void setInstructor(String instructor) {
+		this.instructor = instructor;
+	}
+	public String getLinkedIn() {
+		return linkedIn;
+	}
+	public void setLinkedIn(String linkedIn) {
+		this.linkedIn = linkedIn;
+	}
+	public Courses getCourses() {
+		return courses;
+	}
+	public void setCourses(Courses courses) {
+		this.courses = courses;
+	}
+	
+	
+	
+	
+}
