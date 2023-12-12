8c564eebfb923ba661bab9a13773bd2e1d2e4b06
Bhargav-sdet1 <bhargav.sdet1@gmail.com>
2023-12-12 17:24:53 +0530

    Ecom E2E Scenarios

--- RestAssuredProject/src/test/java/ecomE2E/EcomE2E.java
+++ RestAssuredProject/src/test/java/ecomE2E/EcomE2E.java
@@ -4,12 +4,16 @@ import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
 import io.restassured.specification.RequestSpecification;
+import pojoClassesRestAssured.EcomCreateOrder;
 import pojoClassesRestAssured.EcomLoginDeser;
 import pojoClassesRestAssured.EcomLoginSer;
+import pojoClassesRestAssured.EcomOrders;
 
 import static io.restassured.RestAssured.*;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -28,7 +32,7 @@ public class EcomE2E {
 		.setContentType(ContentType.JSON).build();
 		
 		EcomLoginSer elogin = new EcomLoginSer();
-		elogin.setUserEmail("ram@yopmail.com");
+		elogin.setUserEmail("nov9@yopmail.com");
 		elogin.setUserPassword("Asdfg1@34");
 		
 		RequestSpecification reqLogin=given().spec(req).body(elogin);
@@ -43,7 +47,7 @@ public class EcomE2E {
 	}
 	
 	@Test(priority=2)
-	public void CreateOrder() {
+	public void CreateProduct() {
 		RequestSpecification req=new RequestSpecBuilder().setBaseUri("https://rahulshettyacademy.com")
 		.addHeader("Authorization",token)
 		.addParam("productName", "qwerty")
@@ -57,7 +61,7 @@ public class EcomE2E {
 		
 		String res=given().log().all().spec(req)
 		.when().post("/api/ecom/product/add-product")
-		.then().extract().response().asString();
+		.then().statusCode(201).extract().response().asString();
 	
 		JsonPath js =new JsonPath(res);
 		productId=js.get("productId");
@@ -66,14 +70,41 @@ public class EcomE2E {
 	}
 	
 	@Test(priority=3)
+	public void createOrder() {
+		RequestSpecification req=new RequestSpecBuilder().setBaseUri("https://rahulshettyacademy.com")
+		.addHeader("Authorization", token).setContentType(ContentType.JSON).build();
+		
+		EcomOrders order= new EcomOrders();
+		order.setCountry("India");
+		order.setProductOrderedId(productId);
+		
+		List<EcomOrders> ordersList= new ArrayList<EcomOrders>();
+		ordersList.add(order);
+		
+		EcomCreateOrder createOrders = new EcomCreateOrder();	
+		createOrders.setOrders(ordersList);
+		
+		String response=given().log().all().spec(req).body(createOrders)
+		.when().post("/api/ecom/order/create-order")
+		.then().log().all().extract().response().asString();
+		
+		JsonPath js = new JsonPath(response);
+		js.get("orders");
+		String ProductId=js.get("productOrderId[0]");
+		
+		Assert.assertEquals(productId,ProductId);
+		
+	}
+	
+	@Test(priority=4)
 	public void DeleteProduct() {
 		
 		RequestSpecification req=new RequestSpecBuilder().setBaseUri("https://rahulshettyacademy.com")
 		.addHeader("Authorization", token).build();
 		
 		
-		String response=given().log().all().spec(req)
-		.when().delete("/api/ecom/product/delete-product/"+productId+"")
+		String response=given().pathParam("productId", productId).log().all().spec(req)
+		.when().delete("/api/ecom/product/delete-product/{productId}")
 		.then().extract().response().asString();
 		
 		JsonPath js = new JsonPath(response);
---
+++ RestAssuredProject/src/test/java/pojoClassesRestAssured/EcomCreateOrder.java
@@ -0,0 +1,20 @@
+package pojoClassesRestAssured;
+
+import java.util.List;
+
+public class EcomCreateOrder {
+	
+	public List<EcomOrders> orders;
+
+	public List<EcomOrders> getOrders() {
+		return orders;
+	}
+
+	public void setOrders(List<EcomOrders> orders) {
+		this.orders = orders;
+	}
+
+	
+	
+
+}
