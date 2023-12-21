916944562be214282dd0957c65b9091d92beb20b
Bhargav-sdet1 <bhargav.sdet1@gmail.com>
2023-12-21 20:32:32 +0530

    21/12

--- RestAssuredProject/src/test/java/stepDefinitions/AddPlace.java
+++ RestAssuredProject/src/test/java/stepDefinitions/AddPlace.java
@@ -3,10 +3,14 @@ package stepDefinitions;
 import java.util.ArrayList;
 import java.util.List;
 import static io.restassured.RestAssured.*;
+import static org.junit.Assert.assertEquals;
+
 import io.cucumber.java.en.*;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
 import pojoClasses.AddDetails;
@@ -15,7 +19,9 @@ import pojoClasses.Location;
 public class AddPlace {
 	AddDetails ad; 
 	Location loc;
-	
+	RequestSpecification request;
+	ResponseSpecification res;
+	Response response;
 	
 	@Given("Add place Payload")
 	public void add_place_payload() {
@@ -40,25 +46,32 @@ public class AddPlace {
 		RequestSpecification req=new RequestSpecBuilder().setBaseUri("https://rahulshettyacademy.com")
 		.setContentType(ContentType.JSON).build();
 		
-		RequestSpecification request= given().spec(req).body(ad);
-		
-		ResponseSpecification res=new ResponseSpecBuilder().expectStatusCode(200)
+		res=new ResponseSpecBuilder().expectStatusCode(200)
 				.expectContentType(ContentType.JSON).build();
+		
+		request= given().log().all().spec(req).body(ad);
+		
+		
 		
 	}
 
 	@When("user calls {string} API with POST http request")
 	public void user_calls_api_with_post_http_request(String string) {
-	    
+		response=request.when().post("/maps/api/place/add/json")
+	    .then().log().all().spec(res).extract().response();
 	}
 
 	@Then("Verify API call got success with status code {int}")
-	public void verify_api_call_got_success_with_status_code(Integer int1) {
-	    
+	public void verify_api_call_got_success_with_status_code(Integer statusCode) {
+	    assertEquals(response.getStatusCode(), 200);
+		
 	}
 
 	@Then("{string} in response body is {string}")
-	public void in_response_body_is(String string, String string2) {
-	    
+	public void in_response_body_is(String key, String value) {
+	  String jsonResponse=  response.asString();
+	  JsonPath js= new JsonPath(jsonResponse);
+	  js.get(key.toString());
+	  assertEquals(js.get(key.toString()), value);
 	}
 }
