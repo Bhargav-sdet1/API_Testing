d97f607f6044f270389051f4d64e1c9f2cc45122
Bhargav-sdet1 <bhargav.sdet1@gmail.com>
2024-01-10 12:53:18 +0530

    10/1

--- JavaPrograms/src/javaPractise/CharCount.java
+++ JavaPrograms/src/javaPractise/CharCount.java
@@ -5,19 +5,20 @@ import java.util.HashMap;
 public class CharCount {
 
 	public static void main(String[] args) {
-		String name= "sita";
-		char ch[]=name.toCharArray();
-		HashMap<Character,Integer> hm = new HashMap<Character,Integer>();
+		String name= "Bhargav";
+		char ch[]= name.toCharArray();
+		
+		HashMap<Character,Integer> hm= new HashMap<Character,Integer>();
 		
 		for(char c:ch) {
-			if(hm.containsKey(c)){
+			if(hm.containsKey(c)) {
 				hm.put(c, hm.get(c)+1);
 			}
 			else {
 				hm.put(c, 1);
 			}
 		}
-		System.out.print(hm);
+		System.out.println(hm);
 		
 
 	}
