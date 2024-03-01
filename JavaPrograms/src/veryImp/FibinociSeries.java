ddabb79afb5c4968e9d28cf7c8afd7ab924420f2
Bhargav-sdet1 <bhargav.sdet1@gmail.com>
2024-03-01 19:29:55 +0530

    1/3/24

--- JavaPrograms/src/veryImp/FibinociSeries.java
+++ JavaPrograms/src/veryImp/FibinociSeries.java
@@ -6,11 +6,11 @@ public class FibinociSeries {
 		int count = 10;
 		int a1 = 0;
 		int a2 = 1;
-		int a3 = 0;
+		int a3;
 
 		System.out.print(a1 + " " + a2);
 		for (int i = 2; i < count; ++i) {
-			a3 = a2 + a1;
+			a3 = a1 + a2;
 			a1 = a2;
 			a2 = a3;
 			System.out.print(" "+a3);
